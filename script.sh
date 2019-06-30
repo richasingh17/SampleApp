a3f043cb29623ef88a6d1196fc7309808dd63c91
richasingh17 <richasingh.1707@gmail.com>
2019-06-30 18:40:41 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,10 @@
+aws configure set region us-east-2
+export PASSWORD="$(aws ecr get-login --no-include-email --region us-east-2 | awk '{print $6}')"
+docker login -u AWS -p $PASSWORD https://855754041257.dkr.ecr.us-east-2.amazonaws.com
+aws ecr create-repository --repository-name appimage
+docker rmi 855754041257.dkr.ecr.us-east-2.amazonaws.com/appimage:latest
+docker rmi appimage:latest
+docker build -t appimage .
+docker tag appimage:latest 855754041257.dkr.ecr.us-east-2.amazonaws.com/appimage:latest
+docker system prune -f
+docker push 855754041257.dkr.ecr.us-east-2.amazonaws.com/appimage:latest
